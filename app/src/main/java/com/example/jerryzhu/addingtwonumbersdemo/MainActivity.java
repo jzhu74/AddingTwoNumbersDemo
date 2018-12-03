fb28c9ccbeb07deb12a8f8321b9e9b2b0373d551
jerryzhu <jzhu74@illinois.edu>
2018-12-02 23:31:44 -0600

    Edited the activity xml

--- app/src/main/java/com/example/jerryzhu/addingtwonumbersdemo/MainActivity.java
+++ app/src/main/java/com/example/jerryzhu/addingtwonumbersdemo/MainActivity.java
@@ -20,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
     public static double logGamma(double x) {
         double tmp = (x - 0.5) * Math.log(x + 4.5) - (x + 4.5);
         double ser = 1.0 + 76.18009173    / (x + 0)   - 86.50532033    / (x + 1)
-            + 24.01409822    / (x + 2)   -  1.231739516   / (x + 3)
-            +  0.00120858003 / (x + 4)   -  0.00000536382 / (x + 5);
+                + 24.01409822    / (x + 2)   -  1.231739516   / (x + 3)
+                +  0.00120858003 / (x + 4)   -  0.00000536382 / (x + 5);
         return tmp + Math.log(ser * Math.sqrt(2 * Math.PI));
     }
     public static double gamma(double x) { return Math.exp(logGamma(x)); }
@@ -41,10 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>();
-        for (int i = 0; i < 75; i++) {
+        for (int i = 0; i < 55; i++) {
             x = x  + 0.1;
             y = gamma(x);
-            series.appendData(new DataPoint(x,y), true, 7575);
+            series.appendData(new DataPoint(x,y), true, 55);
         }
         graph.addSeries(series);
 
@@ -68,5 +68,3 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
-
-
