9fee273f773283984c84fa9a449ec74bdaf2006b
jerryzhu <jzhu74@illinois.edu>
2018-12-03 20:33:26 -0600

    exponential and normal added

--- app/src/main/java/com/example/jerryzhu/addingtwonumbersdemo/Activity2.java
+++ app/src/main/java/com/example/jerryzhu/addingtwonumbersdemo/Activity2.java
@@ -1,8 +1,8 @@
 package com.example.jerryzhu.addingtwonumbersdemo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,21 +11,10 @@ import android.widget.TextView;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-import java.lang.Math;
 
-import org.w3c.dom.Text;
+public class Activity2 extends AppCompatActivity {
 
 
-public class MainActivity extends AppCompatActivity {
-
-    public static double logGamma(double x) {
-        double tmp = (x - 0.5) * Math.log(x + 4.5) - (x + 4.5);
-        double ser = 1.0 + 76.18009173    / (x + 0)   - 86.50532033    / (x + 1)
-                + 24.01409822    / (x + 2)   -  1.231739516   / (x + 3)
-                +  0.00120858003 / (x + 4)   -  0.00000536382 / (x + 5);
-        return tmp + Math.log(ser * Math.sqrt(2 * Math.PI));
-    }
-    public static double gamma(double x) { return Math.exp(logGamma(x)); }
     LineGraphSeries<DataPoint> series;
 
     private EditText num1;
@@ -35,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_2);
 
         double y,x;
         x = 0;
@@ -44,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         series = new LineGraphSeries<DataPoint>();
         for (int i = 0; i < 55; i++) {
             x = x  + 0.1;
-            y = gamma(x);
+            y = Math.exp(x);
             series.appendData(new DataPoint(x,y), true, 55);
         }
         graph.addSeries(series);
@@ -58,19 +47,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 double number1 = Double.parseDouble(num1.getText().toString());
                 double sum = number1;
-                result.setText("Answer: " + gamma(sum));
+                result.setText("Answer: " + Math.exp(sum));
 
             }
         });
-
-
-
     }
-    public void openActivity2(View view) {
-        Intent intent = new Intent(this, Activity2.class);
+
+    public void openActivity3(View view) {
+        Intent intent = new Intent(this, Activity3.class);
         startActivity(intent);
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 
-
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
 }
--- app/src/main/java/com/example/jerryzhu/addingtwonumbersdemo/Activity3.java
+++ app/src/main/java/com/example/jerryzhu/addingtwonumbersdemo/Activity3.java
@@ -1,6 +1,5 @@
 package com.example.jerryzhu.addingtwonumbersdemo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,21 +10,8 @@ import android.widget.TextView;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
-import java.lang.Math;
 
-import org.w3c.dom.Text;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    public static double logGamma(double x) {
-        double tmp = (x - 0.5) * Math.log(x + 4.5) - (x + 4.5);
-        double ser = 1.0 + 76.18009173    / (x + 0)   - 86.50532033    / (x + 1)
-                + 24.01409822    / (x + 2)   -  1.231739516   / (x + 3)
-                +  0.00120858003 / (x + 4)   -  0.00000536382 / (x + 5);
-        return tmp + Math.log(ser * Math.sqrt(2 * Math.PI));
-    }
-    public static double gamma(double x) { return Math.exp(logGamma(x)); }
+public class Activity3 extends AppCompatActivity {
     LineGraphSeries<DataPoint> series;
 
     private EditText num1;
@@ -35,17 +21,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_3);
 
         double y,x;
         x = 0;
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>();
-        for (int i = 0; i < 55; i++) {
+        for (int i = 0; i < 75; i++) {
             x = x  + 0.1;
-            y = gamma(x);
-            series.appendData(new DataPoint(x,y), true, 55);
+            y = (1/(Math.sqrt(2 * Math.PI))) * Math.exp(-0.5 * Math.pow(x, 2));
+            series.appendData(new DataPoint(x,y), true, 75);
         }
         graph.addSeries(series);
 
@@ -58,19 +44,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 double number1 = Double.parseDouble(num1.getText().toString());
                 double sum = number1;
-                result.setText("Answer: " + gamma(sum));
+                result.setText("Answer: " + (1/(Math.sqrt(2 * Math.PI))) * Math.exp(-0.5 * Math.pow(sum, 2)));
 
             }
         });
-
-
-
     }
-    public void openActivity2(View view) {
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-    }
-
-
 }
